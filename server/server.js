8e2450a103407f2a5c7e04939d967dd79881cfb1
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2024-03-04 13:24:35 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "bhai",
+  password: "root",
+  database: "bhaaas",
 });
 
 connection.connect();
